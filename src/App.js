9f9b4170a8c4d6440d57db292a79221407094212
VishalBhuse <vbhuse1997@gmail.com>
2022-06-02 13:54:42 -0700

    Final Submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Stopwatch from "./Component/Stopwatch";
+import Timer from "./Component/Timer";
+import { GiSandsOfTime } from "react-icons/gi";
+import { BiStopwatch } from "react-icons/bi";
 
 function App() {
+  const [show, setShow] = useState("showtimer");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main_div">
+      <div className="header">
+        <div
+          onClick={() => setShow("showtimer")}
+          className={show === "showtimer" ? "head" : "heading1"}
         >
-          Learn React
-        </a>
-      </header>
+          <GiSandsOfTime />
+          <p>Timer</p>
+        </div>
+
+        <div
+          onClick={() => setShow("showstopwatch")}
+          className={show === "showstopwatch" ? "head2" : "heading2"}
+        >
+          <BiStopwatch />
+          <p>StopWatch</p>
+        </div>
+      </div>
+
+      <div className="App">
+        <div className="timer" hidden={show === "showtimer" ? false : true}>
+          <Timer />
+        </div>
+
+        <div className="timer" hidden={show === "showstopwatch" ? false : true}>
+          <Stopwatch />
+        </div>
+        
+      </div>
     </div>
   );
 }
